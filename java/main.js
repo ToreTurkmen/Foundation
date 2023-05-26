f164ad153f5408020c75bc98d43961b9427a6e1c
Dovran Torayev <68396453+ToreTurkmen@users.noreply.github.com>
2023-05-26 22:09:47 +0200

    new js

--- java/main.js
+++ java/main.js
@@ -1,12 +1,24 @@
-function getPercentage(percent, amount) {
-  //perce = amount * percent / 100
-  var result;
-  result = amount * percent / 100;
-  return result;
-  }
+// function getPercentage(percent, amount) {
+//   //perce = amount * percent / 100
+//   var result;
+//   result = amount * percent / 100;
+//   return result;
+//   }
 
-  //new
-  const getPercentage = (percent, amount) => {
-    return amount * percent / 100;
+  // //new
+  // const getPercentage = (percent, amount) => {
+  //   return amount * percent / 100;
     
+  // }
+
+  var firstName = 'Tore';
+  function accessGlobalScope () {
+    firstName = "Name has been changed";
+  }
+  function localVariableFunction () {
+    var lastName;
+    lastName = "Last name has been changed";
+  }
+  function localVariable (){
+    assdDevProperty = 
   }
